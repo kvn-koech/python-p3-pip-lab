37f0d1ac9d2097c9fcff3722f2988980165a0381
Kevin <kevinkoech007@gmail.com>
2025-10-06 23:45:25 +0300

    Update assertion comment for requests_version test

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -11,8 +11,7 @@ def test_python_version():
 
 
 def test_requests_version():
-    assert requests_version() == "2.27.1"
-
+    assert requests_version() == "2.27.1"  # Changed from "2.27.1"
 
 def test_pytest_version():
     assert pytest_version() == "7.1.3"
